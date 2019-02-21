97989777da84da409dfabc6d4a81972fedb61547
immm007@163.com <ckh881115>
2019-02-21 22:55:10 +0800

    save

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import requests
 import decimal
 from decimal import Decimal
 import pandas as pd
+import numpy as np
 import datetime
 
 
@@ -15,6 +16,7 @@ class ZhangTing:
     def __init__(self, info):
         self.__info = info
         self.split_info()
+        self.__name = self.__info['NAME']
         today = datetime.datetime.today().strftime('%Y%m%d')
         url = self.__url.format(info['CODE'], '2019', today)
         with self.__session.get(url) as response:
@@ -22,6 +24,7 @@ class ZhangTing:
                 f.write(response.content)
             self.__detail = pd.read_excel('tmp.xls')
             self.analyze_detail()
+            self.remove_extra()
             
     @property
     def detail(self):
@@ -33,7 +36,7 @@ class ZhangTing:
 
     def to_series(self):
         ret = pd.Series(self.__info)
-        ret.name = self.__info['NAME']
+        ret.name = self.__name
         return ret
     
     def split_info(self):
@@ -41,6 +44,10 @@ class ZhangTing:
         self.__info['PROFIT'] = mfratio['MFRATIO2']
         self.__info['REVENUE'] = mfratio['MFRATIO10']
         del self.__info['MFRATIO']
+        if 'ANNOUNMT' in self.__info:
+            content = self.__info['ANNOUNMT']
+            self.__info['ANNOUNMT'] = content[0]['ANNOUNMT2']
+        
     
     @classmethod
     def ztj(cls, price):
@@ -58,13 +65,16 @@ class ZhangTing:
         _open = self.__info['OPEN']
         close = self.__info['PRICE']
         ztj = self.ztj(y_close)
-        self.__info['IS_OPEN_ZT'] = _open == ztj
-        self.__info['IS_CLOSE_ZT'] = close == ztj
+        self.__info['一字版'] = _open == ztj
+        self.__info['涨停收盘'] = close == ztj
+        cjj = self.__detail['成交价']
+        cjsj = self.__detail['成交时间']
+        cjl = self.__detail['成交量（手）']
 # 计算首次上板时间
         scsb = None
         for index in self.__detail.index:
             if self.__detail['成交价'][index] == ztj:
-                self.__info['SCSB'] = self.__detail['成交时间'][index]
+                self.__info['首次上板'] = self.__detail['成交时间'][index]
                 scsb = index
                 break
 # 计算是否一次封死
@@ -73,33 +83,53 @@ class ZhangTing:
             if self.__detail['成交价'][index] != ztj:
                 ycfs = False
                 break
-        self.__info['YCFS'] = ycfs
+        self.__info['一次封死'] = ycfs
 # 计算首次开板时间
         sckb = None
         if not ycfs:
             for index in self.__detail.index[scsb+1:]:
                 if self.__detail['成交价'][index] != ztj:
-                    self.__info['SCKB'] = self.__detail['成交时间'][index]
+                    self.__info['首次开板'] = self.__detail['成交时间'][index]
                     sckb = index
                     break
         else:
-            self.__info['SCKB'] = None
+            self.__info['首次开板'] = np.NAN
 # 计算最后一次回封时间
         zhhf = None
-        if self.__info['IS_CLOSE_ZT']:
+        if not self.__info['一次封死'] and self.__info['涨停收盘']:
+            last = None
             for index in reversed(self.__detail.index):
                 if self.__detail['成交价'][index] == ztj:
+                    last = index
                     continue
                 else:
-                    self.__info['ZHHF'] = self.__detail['成交时间'][index+1]
+                    self.__info['最后回封'] = self.__detail['成交时间'][last]
+                    break
         else:
-            self.__info['ZHHF'] = None
+            self.__info['最后回封'] = np.nan
 # 统计筹码分布
         hp = self.cal_price(y_close, 0.07)
-        sum_vol = self.__detail['成交量'].sum()
-        hp_vol = self.__detail['成交量'][self.__detail['成交价'] >= hp].sum()
+        sum_vol = cjl.sum()
+        hp_vol = self.__detail[cjj >= hp]['成交量（手）'].sum()
+        vol_percent = hp_vol / sum_vol
+        self.__info['高价比例'] = vol_percent
 
-
+    def remove_extra(self):
+        del self.__info['FIVE_MINUTE']
+        del self.__info['CODE']
+        del self.__info['HIGH']
+        del self.__info['LB']
+        del self.__info['LOW']
+        del self.__info['MFSUM']
+        del self.__info['NAME']
+        del self.__info['OPEN']
+        del self.__info['SNAME']
+        del self.__info['UPDOWN']
+        del self.__info['WB']
+        del self.__info['YESTCLOSE']
+        del self.__info['NO']
+#        if 'ANNOUNMT' in self.__info:
+#            del self.__info['ANNOUNMT']
 
 class Searcher:
     def __init__(self):
@@ -130,3 +160,5 @@ class Searcher:
         
 s = Searcher()
 a = s.query()
+today = datetime.datetime.today().strftime('%Y%m%d')
+a.to_excel(today+'.xls')
